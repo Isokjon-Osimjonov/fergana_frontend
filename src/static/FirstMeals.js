e43a1762bbaa27dcd941142dc1441c335c8555ed
Isokjon Osimjonov <isokjonosimjonov@MacBook-Air--Isokjon.local>
2023-06-15 12:06:41 +0900

    d

--- src/static/FirstMeals.js
+++ src/static/FirstMeals.js
@@ -1,15 +1,16 @@
 import Lagmonq from '../assets/lagq.png';
+import Shorva from '../assets/shorva.png'
 export const FIRST_MEALS_STATIC = [
   {
     id: 0,
     name: "Qovurma Lag'mon",
-    price: "11.000",
-    image:Lagmonq,
+    price: '11.000',
+    image: Lagmonq,
   },
   {
     id: 1,
     name: "Qovurma Lag'mon",
-    price: "10.000",
-    image: '',
+    price: '10.000',
+    image: Shorva,
   },
 ];
